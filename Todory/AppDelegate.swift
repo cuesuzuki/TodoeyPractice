290d869a5c1dcbb0c5b06c52a9b9f266f6ad1484
yutaka suzuki <suzukiyutaka@cue-air.local>
2019-03-21 20:08:51 +0900

    Hoe to find CoreData & BackendDataBase.

--- Todory/AppDelegate.swift
+++ Todory/AppDelegate.swift
@@ -7,53 +7,52 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    // データ保存の方法：ホームボタンでアプリを終了するのを保存し、別のアプリに入る
-    //バックグラウンドで実行中のプロセスでもメモリを使っている
-    //メモリを有効活用するための処理
-    //アプリのライフサイクル：起動→画面に表示→背景へ→終了してリソース回収
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-
         
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        
-        //アプリと背景。アプリがスクリーンから消えたときに、バックグラウンドに移動したとき
-        //スマホのメモリに残ってる。問題はアプリ終了するとき
-        print("アプリがバックグラウンドへ移動-applicationDidEnterBackground")
-        
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
+ 
     func applicationWillTerminate(_ application: UIApplication) {
-        // アプリが終了したときの処理
-        //終了ー再起動するとアイテムが消えてしまう。
-        //永続的ストレージがないため、アプリ起動中にリストに追加されるだけ
-        //いくつかの選択肢あるが、小さなデータを永続化するだけならUserDefaultを使う
-        print("アプリが終了したときの処理-applicationWillTerminate")
-    }
 
+        self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 
 }
 
--- Todory/Controllers/TodoListViewController.swift
+++ Todory/Controllers/TodoListViewController.swift
@@ -5,44 +5,29 @@
 //  Created by yutaka suzuki on 2019/03/15.
 //  Copyright © 2019 yutaka suzuki. All rights reserved.
 
-//sec 233:カスタムオブジェクトを保存する（UserDefault以外の方法）
-
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    // CoreDataのfilePathを作成し、グローバル定数にする
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-//    let defaults = UserDefaults.standard　のかわりに独自ファイルパスを作成
-    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
-
-/*  Optional(file:///Users/suzukiyutaka/Library/Developer/CoreSimulator/Devices/4FD55680-DD64-428E-84EA-5F31216B592A/data/Containers/Data/Application/EA8EDD7F-C73D-455D-8351-A9ED43FF2675/Documents/)
- */
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-        loadItems()
-        
-
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
-//            itemArray = items
-//
-//        }
+//        loadItems()
         
     }
 
     // MARK - TableView Datasouece Methods
-    // セクション行数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    // セルの中身
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
@@ -51,22 +36,17 @@ class TodoListViewController: UITableViewController {
 
         cell.textLabel?.text = item.title
         
-        //"turnary operator" ==>
-        //value = condition ? valueIfTrue : valueIfFalse
-        
         cell.accessoryType = item.done ? .checkmark : .none
 
         return cell
-        
     }
     
-    // セクション数
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     //MARK - TableView Delegate Methods
-    // タップすると（indexPathの行番号を表示）するデリゲートメソッド
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
    
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
@@ -79,20 +59,19 @@ class TodoListViewController: UITableViewController {
     
     //MARK - Add New Items
     
-    //①アラート表示させるためのバーボタンを追加
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
         
-        //②アクションとテキストフィールドを持つアラートを作成
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
 
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //persistentContainerを有効にするためのdelegate記述
+
             
-            // UserDefault以外のアイテムはUserDefaultに保存しない！
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -111,30 +90,28 @@ class TodoListViewController: UITableViewController {
     //MARK - Model Manupulation Methods
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
+
         }
-        
-        // ④これを書かないと更新（表示）されない！
+
         self.tableView.reloadData()
         
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            }catch {
-                print("Error decoding item array, \(error)")
-            }
-    }
-    
-}
+//    func loadItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!){
+//            let decoder = PropertyListDecoder()
+//            do {
+//            itemArray = try decoder.decode([Item].self, from: data)
+//            }catch {
+//                print("Error decoding item array, \(error)")
+//            }
+//    }
+//
+//}
 
 }
